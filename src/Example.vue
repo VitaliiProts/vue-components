87b4c4c071202ef7d74f1d7d874c550b61c7b87f
v.prots <vitalikprocj@gmail.com>
2018-02-23 01:26:18 +0200

    add components [props]

---
+++ src/Example.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2>{{ message }}</h2>
+    <app-input :msg="message" @messChange="message = $event"></app-input>
+  </section>
+</template>
+
+<script>
+import Input from './Field.vue'
+
+  export default {
+  data () {
+    return {
+      message: 'New component Example!'
+    }
+  },
+    components: {
+      'app-input': Input
+    }
+  }
+</script>
---
+++ src/Field.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h3>This is Field!</h3>
+    <input type="text" :value="msg" @input="changeMess">
+    <p>{{ mess }}</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        mess: ''
+      }
+    },
+    methods: {
+      changeMess: function (event) {
+        this.mess = event.target.value;
+        this.$emit('messChange', this.mess);
+      }
+    },
+    props: ['msg']
+  }
+</script>
